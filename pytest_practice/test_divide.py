5d9af26eada450d699a99a5a288e467f33b39cc5
mayuri-dangare <mayuri.dangare@clairvoyantsoft.com>
2022-08-30 14:41:10 +0530

    pytest basics

---
+++ pytest_practice/test_divide.py
@@ -0,0 +1,12 @@
+import pytest
+
+@pytest.fixture
+def input_value():
+    input = 50
+    return input
+
+def test_divisible_by_4(input_value):
+    assert input_value % 4 == 0
+
+def test_divisible_by_7(input_value):
+    assert input_value % 7 == 0
