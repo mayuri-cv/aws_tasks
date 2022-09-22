6b201f13f6284a4770ea849cf462af832e3b9d31
mayuri-dangare <mayuri.dangare@clairvoyantsoft.com>
2022-09-22 17:28:42 +0530

    Glue job to copy data from s3 to dynamodb

---
+++ aws_services/s3-to-dynamodb-glue.py
@@ -0,0 +1,47 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Data Catalog table
+DataCatalogtable_node1 = glueContext.create_dynamic_frame.from_catalog(
+    database="zipcodesdb",
+    table_name="tablefors3todynamodb",
+    transformation_ctx="DataCatalogtable_node1",
+)
+#id,zipcode,type,city,state,population
+# Map the source field names and data types to target values. The values below are hypothetical - replace this with the schema of your source database.
+Mapped = ApplyMapping.apply(frame=DataCatalogtable_node1, mappings=[
+    ("id", "bigint", "id", "string"),
+    ("zipcode", "bigint", "zipcode", "string"),
+    ("type", "string", "type", "string"),
+    ("city", "string", "city", "string"),
+    ("state", "string", "state", "string"),
+    ("population", "bigint", "population", "string")
+],
+    transformation_ctx="Mapped")
+
+# Write to target DynamoDB table. Replace <MyRegion> and <MyDynamoDBTable> with region and table name respectively
+glueContext.write_dynamic_frame_from_options(
+    frame=Mapped,
+    connection_type="dynamodb",
+    connection_options={
+        "dynamodb.region": "us-east-1",
+        "dynamodb.output.tableName": "zipcodes",
+        "dynamodb.throughput.write.percent": "1.0"
+    }
+)
+
+
+job.commit()
+
+
