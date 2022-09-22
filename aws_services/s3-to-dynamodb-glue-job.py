88d1d3013a317367ce6d64f76a5de20a7ffd9d21
mayuri-dangare <mayuri.dangare@clairvoyantsoft.com>
2022-09-22 17:38:49 +0530

    Copy table from s3 to dynamodb using aws glue service

--- aws_services/s3-to-dynamodb-glue-job.py
+++ aws_services/s3-to-dynamodb-glue-job.py
@@ -18,8 +18,10 @@ DataCatalogtable_node1 = glueContext.create_dynamic_frame.from_catalog(
     table_name="tablefors3todynamodb",
     transformation_ctx="DataCatalogtable_node1",
 )
-#id,zipcode,type,city,state,population
-# Map the source field names and data types to target values. The values below are hypothetical - replace this with the schema of your source database.
+
+# id,zipcode,type,city,state,population
+# Map the source field names and data types to target values.
+
 Mapped = ApplyMapping.apply(frame=DataCatalogtable_node1, mappings=[
     ("id", "bigint", "id", "string"),
     ("zipcode", "bigint", "zipcode", "string"),
@@ -28,9 +30,9 @@ Mapped = ApplyMapping.apply(frame=DataCatalogtable_node1, mappings=[
     ("state", "string", "state", "string"),
     ("population", "bigint", "population", "string")
 ],
-    transformation_ctx="Mapped")
+     transformation_ctx="Mapped")
 
-# Write to target DynamoDB table. Replace <MyRegion> and <MyDynamoDBTable> with region and table name respectively
+# Write to target DynamoDB table.
 glueContext.write_dynamic_frame_from_options(
     frame=Mapped,
     connection_type="dynamodb",
@@ -41,7 +43,4 @@ glueContext.write_dynamic_frame_from_options(
     }
 )
 
-
 job.commit()
-
-
