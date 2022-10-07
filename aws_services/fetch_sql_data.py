9430e004332000b3be47cc6f4e117580da1940cc
mayuri-dangare <mayuri.dangare@clairvoyantsoft.com>
2022-10-07 12:17:08 +0530

    fetch mysql data using connection and config file

--- aws_services/fetch_sql_data.py
+++ aws_services/fetch_sql_data.py
@@ -1,22 +1,30 @@
 from mysql.connector import connect
 import pandas as pd
 from pyspark.sql import SparkSession
-
+import configparser
+file = '../resources/config.ini'
+config = configparser.ConfigParser()
+config.read(file)
 appName = "PySpark MySQL Example - via mysql.connector"
 master = "local"
 
 spark = SparkSession.builder.master(master).appName(appName).getOrCreate()
-
+user = config.get('fetch_sql_data_db', 'user')
+db = config.get('fetch_sql_data_db', 'database')
+table = config.get('fetch_sql_data_db', 'table-name')
+host = config.get('fetch_sql_data_db', 'host')
+password = config.get('fetch_sql_data_db', 'password')
+port = config.get('fetch_sql_data_db', 'port')
 # Establish a connection
-conn = connect(user='root',
-               database='glue_test',
-               password='root',
-               host="localhost",
-               port=3306)
+conn = connect(user=user,
+               database=db,
+               password=password,
+               host=host,
+               port=port)
 
 cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM emp")
+cursor.execute(f"SELECT * FROM {table}")
 
 result = cursor.fetchall()
 
